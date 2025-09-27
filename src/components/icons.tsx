d64ac652586d3e294387c53946cf13eb722fe93e
Luffy.D.Monkey <luffy.d.monkey01010101@gmail.com>
2025-09-27 17:43:21 +0000

    A circular logo made of leaves, arranged neatly to form a complete circl

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,13 +4,30 @@ export function CivicSolveLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 256"
+        viewBox="0 0 100 100"
         width="1em"
         height="1em"
         {...props}
     >
-        <g fill="currentColor">
-            <path d="M128 24a104 104 0 1 0 104 104A104.12 104.12 0 0 0 128 24Zm45.66 154.34a8 8 0 0 1-11.32 11.32L112 139.31l-10.34 10.35a8 8 0 0 1-11.32-11.32L112 116.69l30.34-30.35a8 8 0 0 1 11.32 11.32L123.31 128Z"/>
+        <g fill="currentColor" transform="translate(50 50)">
+            <g transform="rotate(0)">
+                <path d="M 0 -45 C 15 -45, 15 -10, 0 -10 C -15 -10, -15 -45, 0 -45" fill="#66BB6A" />
+            </g>
+            <g transform="rotate(60)">
+                <path d="M 0 -45 C 15 -45, 15 -10, 0 -10 C -15 -10, -15 -45, 0 -45" fill="#81C784" />
+            </g>
+            <g transform="rotate(120)">
+                <path d="M 0 -45 C 15 -45, 15 -10, 0 -10 C -15 -10, -15 -45, 0 -45" fill="#A5D6A7" />
+            </g>
+            <g transform="rotate(180)">
+                <path d="M 0 -45 C 15 -45, 15 -10, 0 -10 C -15 -10, -15 -45, 0 -45" fill="#66BB6A" />
+            </g>
+            <g transform="rotate(240)">
+                <path d="M 0 -45 C 15 -45, 15 -10, 0 -10 C -15 -10, -15 -45, 0 -45" fill="#81C784" />
+            </g>
+            <g transform="rotate(300)">
+                <path d="M 0 -45 C 15 -45, 15 -10, 0 -10 C -15 -10, -15 -45, 0 -45" fill="#A5D6A7" />
+            </g>
         </g>
     </svg>
   );
